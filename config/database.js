2924bc6bcd02f809dc3b3f7e88a5e891f0f49ee0
Beta Bilim <ultracgartist@gmail.com>
2022-07-11 02:24:58 +0400

    gitignore ignor edildi ve database.js melumati yenilendi

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi-baxaq'),
         username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        password: env('DATABASE_PASSWORD', 'Baza2029@'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
